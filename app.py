0dae7daa5a2a681657d5faa9afb0fe4e4cc4223e
Tim <timvrijn01@gmail.com>
2025-11-06 16:59:58 +0100

    Test2

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #port = int(os.environ.get("PORT", 5000))  # pak PORT van Render, anders 5000 lokaal
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # pak PORT van Render, anders 5000 lokaal
     #app.run(host="0.0.0.0", port=port, debug=False)
